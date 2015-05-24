5718ecd55e44d3ad27074f2b8cd3e13256ad7676
parroty <parroty00@gmail.com>
2015-05-24 23:50:44 +0900

    Fix tests for basic authentication

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,9 +1,35 @@
 require "test_helper"
 
 describe TopicsController do
+  before do
+    @user = "DUMMY_USER"
+    @pass = "DUMMY_PASS"
+
+    ENV.expects(:[]).with("THOUGHT_MAPPER_USER").returns(@user)
+    ENV.expects(:[]).with("THOUGHT_MAPPER_PASS").returns(@pass)
+
+    Rails.stubs(:env).returns("production".inquiry)
+  end
+
   describe "basic_authentication" do
-    it "gets index page fails with authentication error" do
-      Rails.stub(:env, ActiveSupport::StringInquirer.new("production")) do
+    describe "with valid user and pass" do
+      before do
+        @original = @request.env["HTTP_AUTHORIZATION"]
+        @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{@user}:#{@pass}")
+      end
+
+      after do
+        @request.env["HTTP_AUTHORIZATION"] = @original
+      end
+
+      it "succeeds to get page" do
+        get :index
+        assert_response 200
+      end
+    end
+
+    describe "without valid user and pass" do
+      it "fails to get page" do
         get :index
         assert_response 401
       end
